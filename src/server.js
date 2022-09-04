66041dcf9efc7925bd5d739c07a6d2a23da204ad
yogi <moh.yogi10065@gmail.com>
2022-09-04 16:58:08 +0700

    implementation database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NoteService = require('./services/inMemory/NotesServices');
+const NoteService = require('./services/postgres/NoteServices');
 const NotesValidator = require('./validator/notes')
 require('dotenv').config()
 
